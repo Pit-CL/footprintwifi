78c4e237aa2ed05b7da787355f81b62984d64dd8
rafael <rafaelfariaspoblete@gmail.com>
2021-07-11 02:51:32 -0400

    cambios python

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Tarea BigData Analytics.
+
+Reunir información sobre footprint wifi y en base a la información predecir.
+Alumno: Rafael Farias.
+"""
 import findspark
 from pyspark import SparkContext, SparkConf, SQLContext
 import csv
@@ -17,64 +23,32 @@ sc = SparkContext.getOrCreate(conf=conf)
 sqlContext = SQLContext(sc)
 
 # TODO: Tratar de hacer un loop mas automatico para la lectura.
-# Carpeta donde están guardados los archivos.
 FilePath = '/home/rafa/Dropbox/Linux_MDS/BDAnalytics/sprint1/data'
 
-# Nombres de los archivo.
 FileName1 = 'wifi_2017.csv'
 FileName2 = 'wifi_2018.csv'
 FileName3 = 'wifi_2019.csv'
 
-# Combinando
 FullPath1 = FilePath + '/' + FileName1
 FullPath2 = FilePath + '/' + FileName2
 FullPath3 = FilePath + '/' + FileName3
 
-# Dataframes
 df_2017 = sqlContext.read.csv(FullPath1, header=True)
 df_2018 = sqlContext.read.csv(FullPath2, header=True)
 df_2019 = sqlContext.read.csv(FullPath3, header=True)
 
-# Mostrando los data frame
-df_2017.show()
-df_2018.show()
-df_2019.show()
-
-# Muestro el esquema.
-df_2017.printSchema()
-df_2018.printSchema()
-df_2019.printSchema()
-
-
-# Continuo con los csv
-
-# Cambiando el nombre a la columna data_source por data.
 df_2017 = df_2017.withColumnRenamed('data_source', 'data')
 
-df_2017.printSchema()
-
-df_2017.show()
-
-# Noto que hay columnas extras en un dataset, en primera instancia las elimino.
 df_2017 = df_2017.drop('range', 'created')
-
-df_2017.printSchema()
-
-df_2017.show()
 
 # Ahora ordeno las columnas para que todas tengan el mismo orden.
 df_2017 = df_2017.select('id', 'bssid', 'lat', 'lon', 'updated', 'data')
 
-df_2017.printSchema()
+df_unidos = (df_2017.union(df_2018)).union(df_2019)
+print('El dataframe que contiene todos los csv es el siguiente:\n')
+df_unidos.show(truncate=False)
 
-df_2017.show()
-
-# Ahora uno los df_2017, df_2018 y df_2019.
-df_2017_2018_2019 = (df_2017.union(df_2018)).union(df_2019)
-df_2017_2018_2019.show(truncate=False)
-
-# Elimino las columnas innecesarias.
-df_2017_2018_2019 = df_2017_2018_2019.drop('updated', 'data')
+df_unidos = df_unidos.drop('updated', 'data')
 
 # TODO: Preguntarle al profe si es que debo hacer un drop o no.
 # Elimino las duplicadas según bssid
@@ -82,47 +56,40 @@ df_2017_2018_2019 = df_2017_2018_2019.drop('updated', 'data')
 # print('bssid únicas : '+str(df_all.count()))
 # df_all.show(truncate=False)
 
-df_all = df_2017_2018_2019
-
 # Ahora creo el df solo con la RM según coordenadas de google maps.
-df_stgo = df_all.filter((df_all.lat >= -33.65) &
-                        (df_all.lat <= -33.28) &
-                        (df_all.lon >= -70.81) &
-                        (df_all.lon <= -70.50))
-df_stgo.show(truncate=False)
+df_stgo = df_unidos.filter((df_unidos.lat >= -33.65) &
+                           (df_unidos.lat <= -33.28) &
+                           (df_unidos.lon >= -70.81) &
+                           (df_unidos.lon <= -70.50))
 
 # Credo el df final de Stgo.
 # Separo la columna bssid en una que contendrá Media_mac y otra que contendrá
 # Id_fabricante.
 df_stgo = df_stgo.withColumn('Media_mac', expr('substring(bssid,1,length(bssid)-6)')).withColumn(
     'Id_fabricante', expr('substring(bssid,7,length(bssid)-6)')).drop('bssid')
+print('El dataframe de Santiago es el siguiente:\n')
 df_stgo.show()
 
-# Ordeno el dataset por lat.
-# dropDupDF.sort(desc('lat')).show()
-
 # Ahora trabajo con el archivo de texto.
-# Inicio el diccionario que contendra el id y el nombre del vendor.
-result = dict()
+dict_vendor_id = dict()
 
 for lig in open('/home/rafa/Dropbox/Linux_MDS/BDAnalytics/sprint1/data/'
                 'oui.txt'):
     if 'base 16' in lig:
         num, sep, txt = lig.strip().partition('(base 16)')
-        result[num.strip()] = txt.strip()
+        dict_vendor_id[num.strip()] = txt.strip()
 
 # Transformo el diccionario en csv para mejor manipulación.
 with open('/home/rafa/Dropbox/Linux_MDS/BDAnalytics/sprint1/oui.csv',
           'w') as f:
     w = csv.writer(f)
-    w.writerows(result.items())
+    w.writerows(dict_vendor_id.items())
 
 # Creo el df
 df_oui = sqlContext.read.csv('/home/rafa/Dropbox/Linux_MDS/BDAnalytics/'
                              'sprint1/oui.csv',
                              header=False)
-
-# Muestro el df
+print('El dataframe del archivo OUI.txt es el siguiente:\n')
 df_oui.show(truncate=False)
 
 # Ahora me preocupo de revisar los archivos de geolocalización.
@@ -136,9 +103,9 @@ Manzana_Precensal = gpd.read_file('/home/rafa/Dropbox/Linux_MDS/BDAnalytics/'
 # Elimino columnas innecesarias.
 Manzana_Precensal = Manzana_Precensal.drop(['DES_REGI', 'MANZENT', 'COMUNA',
                                             'PROVINCIA', 'DES_PROV', 'REGION',
-                                            'COD_DIS'],
-                                           axis=1)
+                                            'COD_DIS'], axis=1)
 # https://sparkbyexamples.com/pyspark/convert-pandas-to-pyspark-dataframe/
+print('El shape que contiene los datos solicitados es Manzana Precensal:\n')
 print(Manzana_Precensal)
 
 # Construcción de futures.
@@ -147,10 +114,8 @@ print(Manzana_Precensal)
 # en donde Id_fabricante sea idéntico a _c0 del df_oui.
 # TODO: Preguntarle al profe si es necesaria la función.
 df_stgo = df_stgo.join(df_oui).where(df_stgo["Id_fabricante"] == df_oui["_c0"])
-df_stgo.show()
-
-# Elimino la columna _c0
 df_stgo = df_stgo.drop('_c0')
+print('El dataframe con el primer future es el siguiente:\n')
 df_stgo.show(truncate=False)
 
 # Información geográfica (ciudad, comuna, zona censal, manzana)
