d6e3786db97d6f58cdc6c04598ebfaf84d534452
rafael <rafaelfariaspoblete@gmail.com>
2021-07-27 15:37:47 -0400

    s3 differences ready

--- main.py
+++ main.py
@@ -1,26 +1,4 @@
 # -*- coding: utf-8 -*-
-"""Tarea BigData Analytics.
-
-Reunir información sobre footprint wifi y en base a la información predecir
-por ejemplo que Fabricante se instalará en una cierta zona.
-Se recomienda instalar los requerimientos en un entorno aislado ya que son
-librerías que suelen generar conflictos por sus versiones.
-
-Debes descargar desde https://www.mylnikov.org/download los últimos archivos
-del año 2017, 2018 y 2019. Luego debes nombrar los tres archivos como
-wifi_XXXX.csv en donde XXXX corresponde al año , debes descargar el archivo
-out.txt desde http://standards-oui.ieee.org/oui/oui.txt
-
-Dentro del script no olvides indicar la ruta al path que contiene los archivos
-que descargaste, la variable dentro del script está nombrada como FilePath.
-
-Todos estos archivos deben estar guardados en la misma carpeta.
-Por último recuerda que si lo corres en tu máquina local debes tener habilitado
-Spark.
-
-Alumno: Rafael Farias.
-Profesor: Oscar Peredo.
-"""
 from os import truncate
 import findspark
 from pyspark import SparkContext, SparkConf, SQLContext
